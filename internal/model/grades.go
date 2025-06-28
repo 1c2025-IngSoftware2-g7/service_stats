2231d3f5585b66e6fdb46f7dc519df9e8a855253
lucas794 <lgtorres@fi.uba.ar>
2025-06-28 19:55:06 -0300

    Model changed from binding required to optional

--- internal/model/grades.go
+++ internal/model/grades.go
@@ -6,7 +6,7 @@ type Grade struct {
 	StudentID string    `json:"student_id" binding:"required"`
 	CourseID  string    `json:"course_id" binding:"required"`
 	Grade     float64   `json:"grade" binding:"required"`
-	OnTime    bool      `json:"on_time" binding:"required"`
+	OnTime    bool      `json:"on_time"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
